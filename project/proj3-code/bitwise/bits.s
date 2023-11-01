c22a9abeee70cb581a7b100e32ab6594b4696e2f
Hongzheng Li <roymanclee@gmail.com>
2023-11-01 23:04:13 +0800

    initial state of project 3 part 1

---
+++ project/proj3-code/bitwise/bits.s
@@ -0,0 +1,144 @@
+# Read the following instructions carefully
+# You will provide your solution to this part of the project by
+# editing the collection of functions in this source file.
+#
+# Some rules from Project 2 are still in effect for your assembly code here:
+#  1. No global variables are allowed
+#  2. You may not define or call any additional functions in this file
+#  3. You may not use any floating-point assembly instructions
+# You may assume that your machine:
+#  1. Uses two's complement, 32-bit representations of integers.
+
+# isZero - returns 1 if x == 0, and 0 otherwise
+#   Argument 1: x
+#   Examples: isZero(5) = 0, isZero(0) = 1
+#   Rating: 1
+.global isZero
+isZero:
+    movl $2, %eax
+    ret
+
+# bitNor - ~(x|y)
+#   Argument 1: x
+#   Argument 2: y
+#   Example: bitNor(0x6, 0x5) = 0xFFFFFFF8
+#   Rating: 1
+.global bitNor
+bitNor:
+    movl $2, %eax
+    ret
+
+# distinctNegation - returns 1 if x != -x.
+#     and 0 otherwise
+#   Argument 1: x
+#   Rating: 2
+.global distinctNegation
+distinctNegation:
+    movl $2, %eax
+    ret
+
+# dividePower2 - Compute x/(2^n), for 0 <= n <= 30
+#  Round toward zero
+#   Argument 1: x
+#   Argument 2: n
+#   Examples: dividePower2(15,1) = 7, dividePower2(-33,4) = -2
+#   Rating: 2
+.global dividePower2
+dividePower2:
+    movl $2, %eax
+    ret
+
+# getByte - Extract byte n from word x
+#   Argument 1: x
+#   Argument 2: n
+#   Bytes numbered from 0 (least significant) to 3 (most significant)
+#   Examples: getByte(0x12345678,1) = 0x56
+#   Rating: 2
+.global getByte
+getByte:
+    movl $2, %eax
+    ret
+
+# isPositive - return 1 if x > 0, return 0 otherwise
+#   Argument 1: x
+#   Example: isPositive(-1) = 0.
+#   Rating: 2
+.global isPositive
+isPositive:
+    movl $2, %eax
+    ret
+
+# floatNegate - Return bit-level equivalent of expression -f for
+#   floating point argument f.
+#   Both the argument and result are passed as unsigned int's, but
+#   they are to be interpreted as the bit-level representations of
+#   single-precision floating point values.
+#   When argument is NaN, return argument.
+#   Argument 1: f
+#   Rating: 2
+.global floatNegate
+floatNegate:
+    movl $2, %eax
+    ret
+
+# isLessOrEqual - if x <= y  then return 1, else return 0
+#   Argument 1: x
+#   Argument 2: y
+#   Example: isLessOrEqual(4,5) = 1.
+#   Rating: 3
+.global isLessOrEqual
+isLessOrEqual:
+    movl $2, %eax
+    ret
+
+# bitMask - Generate a mask consisting of all 1's between
+#   lowbit and highbit
+#   Argument 1: highbit
+#   Argument 2: lowbit
+#   Examples: bitMask(5,3) = 0x38
+#   Assume 0 <= lowbit <= 31, and 0 <= highbit <= 31
+#   If lowbit > highbit, then mask should be all 0's
+#   Rating: 3
+.global bitMask
+bitMask:
+    movl $2, %eax
+    ret
+
+# addOK - Determine if can compute x+y without overflow
+#   Argument 1: x
+#   Argument 2: y
+#   Example: addOK(0x80000000,0x80000000) = 0,
+#            addOK(0x80000000,0x70000000) = 1,
+#   Rating: 3
+.global addOK
+addOK:
+    movl $2, %eax
+    ret
+
+# floatScale64 - Return bit-level equivalent of expression 64*f for
+#   floating point argument f.
+#   Both the argument and result are passed as unsigned int's, but
+#   they are to be interpreted as the bit-level representation of
+#   single-precision floating point values.
+#   When argument is NaN, return argument
+#   Argument 1: f
+#   Rating: 4
+.global floatScale64
+floatScale64:
+    movl $2, %eax
+    ret
+
+# floatPower2 - Return bit-level equivalent of the expression 2.0^x
+#   (2.0 raised to the power x) for any 32-bit integer x.
+#
+#   The unsigned value that is returned should have the identical bit
+#   representation as the single-precision floating-point number 2.0^x.
+#   If the result is too small to be represented as a denorm, return
+#   0. If too large, return +INF.
+#
+#   Argument 1: x
+#   Rating: 4
+.global floatPower2
+floatPower2:
+    movl $2, %eax
+    ret
---
+++ project/proj3-code/bitwise/bits_test.h
@@ -0,0 +1,17 @@
+#ifndef BITS_TEST_H
+#define BITS_TEST_H
+
+int test_isZero(int x);
+int test_bitNor(int x, int y);
+int test_distinctNegation(int x);
+int test_dividePower2(int x, int n);
+int test_getByte(int x, int n);
+int test_isPositive(int x);
+unsigned test_floatNegate(unsigned uf);
+int test_isLessOrEqual(int x, int y);
+int test_bitMask(int highbit, int lowbit);
+int test_addOK(int x, int y);
+unsigned test_floatScale64(unsigned uf);
+unsigned test_floatPower2(int x);
+
+#endif // BITS_TEST_H
